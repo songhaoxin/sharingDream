f03776e4f1444c2cec8c5b52b70326cc181e2cb2
songhaoxin <songmjian@gmail.com>
2014-08-25 16:18:52 +0800

    Initial Commit

---
+++ sharingDream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sharingDream
+//
+//  Created by song mingjian on 14-8-25.
+//  Copyright (c) 2014年 song mingjian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ sharingDreamTests/sharingDreamTests.swift
@@ -0,0 +1,35 @@
+//
+//  sharingDreamTests.swift
+//  sharingDreamTests
+//
+//  Created by song mingjian on 14-8-25.
+//  Copyright (c) 2014年 song mingjian. All rights reserved.
+//
+
+import XCTest
+
+class sharingDreamTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
