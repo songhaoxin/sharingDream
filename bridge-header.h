43a1d40e66e825074ab236ff73fcf53d9a399fbe
songhaoxin <songmjian@gmail.com>
2014-08-25 16:43:27 +0800

    增加桥结头文件bridge-header.h并且测试是否可以正常使用bridge库中的类

---
+++ bridge-header.h
@@ -0,0 +1,8 @@
+//
+//  bridge-header.h
+//  sharingDream
+//
+//  Created by song mingjian on 14-8-25.
+//  Copyright (c) 2014年 song mingjian. All rights reserved.
+//
+#import "AFNetworking/AFNetworking.h"
